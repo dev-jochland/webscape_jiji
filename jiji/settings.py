d18e118e7254d54cfeada86fe1f047e07a0605bb
Joel Onojason <joel.onojason@outlook.com>
2020-04-29 19:59:06 +0100

    Views updated

--- jiji/settings.py
+++ jiji/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIR = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
-# del DATABASES['default']['OPTIONS']['sslmode']
+del DATABASES['default']['OPTIONS']['sslmode']
